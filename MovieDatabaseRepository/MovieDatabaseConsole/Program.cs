2e1861519ed2dd3ad04b2b9ea7e13d6fe232b02e
colin <cjsteward07@gmail.com>
2022-12-15 03:17:21 -0500

    update

--- MovieDatabaseRepository/MovieDatabaseConsole/Program.cs
+++ MovieDatabaseRepository/MovieDatabaseConsole/Program.cs
@@ -28,11 +28,22 @@ void LoadStartUpData()
 List<Movie> BuildItemCollection()
 {
     List<Movie> initialItems = new List<Movie>();
-    initialItems.Add(new Movie() { Title = "Common Arrow", Genre = "GenreTest", Runtime = 19 });
-    //initialItems.Add(new Movie() { Name = "Dull Sword", Description = "A very old sword" });
-    //initialItems.Add(new Movie() { Name = "Ragged Tunic", Description = "It barely covers the important bits" });
-    //initialItems.Add(new Movie() { Name = "Common Arrow", Description = "A cheap wood arrow" });
-    //initialItems.Add(new Movie() { Name = "Dented Helm", Description = "What happened to the previous owner" });
+    initialItems.Add(new Movie() { Title = "Dumb and Dumber", Genre = "Comedy", Runtime = 19 });
+    initialItems.Add(new Movie() { Title = "Gladiator", Genre = "Action", Runtime = 155 });
+    initialItems.Add(new Movie() { Title = "Top Gun", Genre = "Action", Runtime = 110 });
+    initialItems.Add(new Movie() { Title = "Dumbo", Genre = "Musical", Runtime = 64 });
+    initialItems.Add(new Movie() { Title = "Nightmare Before Christmas", Genre = "Fantasy", Runtime = 76 });
+    initialItems.Add(new Movie() { Title = "Saw", Genre = "Horror", Runtime = 200 });
+    initialItems.Add(new Movie() { Title = "The Lord Of The Rings", Genre = "Fantasy", Runtime = 178 });
+    initialItems.Add(new Movie() { Title = "Batman: The Dark Knight", Genre = "Action", Runtime = 92 });
+    initialItems.Add(new Movie() { Title = "The Mummy", Genre = "Adventure", Runtime = 124 });
+    initialItems.Add(new Movie() { Title = "Jurassic Park", Genre = "Adventure", Runtime = 127 });
+    initialItems.Add(new Movie() { Title = "Terminator 2", Genre = "Thriller", Runtime = 152 });
+    initialItems.Add(new Movie() { Title = "Black Hawk Down", Genre = "Action", Runtime = 152 });
+    initialItems.Add(new Movie() { Title = "Ace Ventura", Genre = "Comedy", Runtime = 86 });
+    initialItems.Add(new Movie() { Title = "Remember The Titans", Genre = "Drama", Runtime = 120 });
+    initialItems.Add(new Movie() { Title = "The Iron Giant", Genre = "Adventure", Runtime = 86 });
+
     return initialItems;
 }
 
